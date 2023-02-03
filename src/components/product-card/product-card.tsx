e0a072220434dd1232646b57ed497f011a98394e
Андрей Куликов <mutoguyver@gmail.com>
2023-02-03 22:19:45 +0300

    feat(MadeInAlfapage): done, using dummy for now

---
+++ src/components/product-card/product-card.tsx
@@ -0,0 +1,28 @@
+import { Typography } from "@alfalab/core-components/typography";
+import { Amount } from "@alfalab/core-components/amount";
+import React from "react";
+import "./product-card.css";
+
+type ProductCardType = {
+	preview: string;
+	title: string;
+	price: number;
+	availability: boolean;
+};
+
+export const ProductCard = ({ preview, title, price, availability }: ProductCardType) => {
+	const inStock = availability ? "в наличии" : "закончились";
+
+	return (
+		<div className="product">
+			<img className="product-img" src={preview} alt={title} />
+			<Typography.Text view="primary-large" weight="bold">
+				{title}
+			</Typography.Text>
+			<Amount value={price} minority={0} currency="RUB" bold="major" />
+			<Typography.Text view="primary-large" weight="bold">
+				{inStock}
+			</Typography.Text>
+		</div>
+	);
+};
--- src/pages/made-in-alfapage.tsx
+++ src/pages/made-in-alfapage.tsx
@@ -1,5 +1,38 @@
+import { Space } from "@alfalab/core-components/space";
+import { Gap } from "@alfalab/core-components/gap";
+import { Typography } from "@alfalab/core-components/typography";
 import { Page } from "components/page";
+import { ProductCard } from "components/product-card";
+import "./styles.css";
+import data from "data/products.json";
 
 export const MadeInAflapage = () => {
-	return <Page />;
+	const products = data.products;
+
+	return (
+		<Page>
+			<div className="made-in-alfapage">
+				<Space>
+					<Typography.Title tag="h1" view="xlarge" weight="bold">
+						Сделано в Альфе
+					</Typography.Title>
+					<Typography.Text view="primary-large" weight="bold">
+						Хотим каждую из этих вещей! Себе, родным и друзьям
+					</Typography.Text>
+				</Space>
+				<Gap size="xl" />
+				<div className="products">
+					{products.map((product) => (
+						<ProductCard
+							key={product.id}
+							preview="https://static.tildacdn.com/stor6531-3139-4435-b365-653562306137/98058636.jpg"
+							title={product.title}
+							price={product.price}
+							availability={product.availability}
+						/>
+					))}
+				</div>
+			</div>
+		</Page>
+	);
 };
