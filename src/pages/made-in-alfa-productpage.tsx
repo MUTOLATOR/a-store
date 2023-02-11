38ce41bbada41e1a48b1ef6031d213dee22eb7ad
Андрей Куликов <mutoguyver@gmail.com>
2023-02-11 08:23:34 +0300

    fix(MadeInAlfaProductpage): fix type

--- src/pages/made-in-alfa-productpage.tsx
+++ src/pages/made-in-alfa-productpage.tsx
@@ -3,20 +3,21 @@ import "./styles.css";
 import data from "data/products.json";
 import { useParams } from "react-router-dom";
 import { Product } from "components/product";
+import { ProductType } from "components/product/product";
 
 export const MadeInAflaProductpage = () => {
 	const { productId } = useParams();
 	const product = data.products.find((product) => product.id === parseInt(productId as string));
-	const { images, title, price, description, availability, colors, sizes, models } = product || {};
+	const { images, title, price, description, availability, colors, sizes, models } = product as ProductType;
 
 	return (
 		<Page>
 			<Product
-				images={images || []}
-				title={title || ""}
-				price={price || 0}
-				description={description || ""}
-				availability={availability || false}
+				images={images}
+				title={title}
+				price={price}
+				description={description}
+				availability={availability}
 				colors={colors}
 				sizes={sizes}
 				models={models}
