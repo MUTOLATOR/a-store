e02b898a856e8d6a8d824cfe437db9932684458f
Андрей Куликов <mutoguyver@gmail.com>
2023-03-18 20:47:34 +0300

    fix(App): change BrowserRouter to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { ErrorBoundary } from "components/error-boundary";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux/es/exports";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as BrowserRouter } from "react-router-dom"; // для публикации на github.pages
 import { store } from "store";
 import { App } from "./components/app";
 import "./global.css";
